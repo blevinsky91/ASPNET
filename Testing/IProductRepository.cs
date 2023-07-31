2c68d9c65132d20d93be87ea72e7eef439c13a55
Brandon Levinsky <blevinsky91@gmail.com>
2023-07-31 15:36:34 -0400

    message

---
+++ Testing/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Testing.Models;
+
+namespace Testing
+{
+	public interface IProductRepository
+	{
+
+        public IEnumerable<Product> GetAllProducts();
+
+    }
+
+	
+}
+
