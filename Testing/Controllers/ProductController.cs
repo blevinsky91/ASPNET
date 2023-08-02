2cdc879157141a7fea8770dcb01f094000040a30
Brandon Levinsky <blevinsky91@gmail.com>
2023-08-02 15:05:04 -0400

    all tasks complete

--- Testing/Controllers/ProductController.cs
+++ Testing/Controllers/ProductController.cs
@@ -62,6 +62,12 @@ namespace Testing.Controllers
             repo.InsertProduct(productToInsert);
             return RedirectToAction("Index");
         }
+
+        public IActionResult DeleteProduct(Product product)
+        {
+            repo.DeleteProduct(product);
+            return RedirectToAction("Index");
+        }
     }
 }
 
